abc9b762e7a512349e38ac89c41640b50bac0ef9
Kirill <kdberdyshev@gmail.com>
2024-11-06 23:29:15 +0300

    Initial commit

---
+++ domain/billboards.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+
+class Billboard(BaseModel):
+    id: Optional[int]
+    cost: float
+    size: float
+    installation_date: Optional[datetime]
+    addres: str
+    quality_indicator: int
+    city: str
+    direction: str
+    billboard_owner_id: int
+
+
+
+class BillboardOwner(BaseModel):
+    id: int
+    last_name: str
+    birthdate: datetime
+    addres: str
+    phone_number: str = Field(pattern=r"^(?:\+7|8)?\s?\(?\d{3}\)?\s?\d{3}[-\s]?\d{2}[-\s]?\d{2}$")
+
+
+class BillboardTenant(BaseModel):
+    id: Optional[int]
+    last_name: str
+    addres: str
+    contract_date: datetime
+    phone_number: str = Field(Field(pattern=r"^(?:\+7|8)?\s?\(?\d{3}\)?\s?\d{3}[-\s]?\d{2}[-\s]?\d{2}$"))
+
+
+class Order(BaseModel):
+    id: Optional[int]
+    registration_date: datetime
+    total_cost: float
+    

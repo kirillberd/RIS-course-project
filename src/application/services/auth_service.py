b8f16c38d77d40a689b6d9df0ed03d76ebdf9882
Kirill <kdberdyshev@gmail.com>
2024-12-01 21:20:08 +0300

    Add user password encoding

--- src/application/services/auth_service.py
+++ src/application/services/auth_service.py
@@ -5,6 +5,7 @@ from domain.user import BaseUser
 from pymysql import IntegrityError
 from infrastructure.exceptions.auth_errors import UserAlreadyExistsError
 import logging
+import bcrypt
 
 module_logger = logging.getLogger()
 
@@ -15,9 +16,16 @@ class AuthService:
 
     def register_user(self, user: BaseUser):
         try:
+            salt = bcrypt.gensalt()
+            password_hashed = self._hash_user_password(user.password)
+            user.password = password_hashed
             query = self.sql_provider.get("create_user.sql", **user.model_dump())
             self.user_repository.add(user, query)
         except IntegrityError:
             raise UserAlreadyExistsError("Пользователь с данным username уже существует.")
         
-        
+
+    def _hash_user_password(self, password: str):
+        salt = bcrypt.gensalt()
+        password_hashed = bcrypt.hashpw(password.encode("utf-8"), salt).decode()
+        return password_hashed

5889b753653a1af71475ed34650306f0d91cb6e7
Kirill <kdberdyshev@gmail.com>
2024-12-03 18:17:03 +0300

    Rework file structure

--- src/infrastructure/api/blueprints/billboards/billboards.py
+++ src/infrastructure/api/blueprints/billboards/billboards.py
@@ -7,11 +7,18 @@ from dependency_injector.wiring import inject, Provide
 from domain.billboards import Billboard
 from application.services.billboard_service import BillboardService
 import logging
+from domain.billboards import BillboardQuery
 
 module_logger = logging.getLogger(__name__)
 
 
 billboard_blueprint = Blueprint("billboard_blueprint", __name__, template_folder="templates", static_folder="static")
+
+
+@billboard_blueprint.route("/", methods=["GET"])
+@auth_required
+def query_menu():
+    return render_template("search_billboards.html")
 
 
 @billboard_blueprint.route("/add", methods=["GET"])
@@ -40,3 +47,58 @@ def add_billboard_handler_post(billboard_service: BillboardService = Provide[Con
     billboard = Billboard.model_validate(billboard_object)
     billboard_service.add_billboard(billboard)
     return render_template("add_billboard_template.html", message="Билборд успешно добавлен.")
+
+@billboard_blueprint.route("/search", methods=["GET"])
+@auth_required
+@inject
+def search_handler(billboard_service: BillboardService = Provide[Container.billboard_service]):
+    query_params = {
+        "city": request.args.get("city") if request.args.get("city") else None,
+        "direction": (
+            request.args.get("direction") if request.args.get("direction") else None
+        ),
+        "cost_min": (
+            request.args.get("cost_min", type=float)
+            if request.args.get("cost_min", type=float)
+            else None
+        ),
+        "cost_max": (
+            request.args.get("cost_max", type=float)
+            if request.args.get("cost_max", type=float)
+            else None
+        ),
+        "size_min": (
+            request.args.get("size_min", type=float)
+            if request.args.get("size_min", type=float)
+            else None
+        ),
+        "size_max": (
+            request.args.get("size_max", type=float)
+            if request.args.get("size_max", type=float)
+            else None
+        ),
+        "min_quality": (
+            request.args.get("min_quality", type=int)
+            if request.args.get("min_quality", type=int)
+            else None
+        ),
+        "address": request.args.get("address") if request.args.get("address") else None,
+        "date_from": (
+            datetime.strptime(request.args.get("date_from"), "%Y-%m-%d")
+            if request.args.get("date_from")
+            else None
+        ),
+        "date_to": (
+            datetime.strptime(request.args.get("date_to"), "%Y-%m-%d")
+            if request.args.get("date_to")
+            else None
+        ),
+    }
+
+    module_logger.info(query_params)
+    query_obj = BillboardQuery.model_validate(query_params)
+    result = billboard_service.get_billboards(query_obj)
+    module_logger.info(result)
+    return render_template("billboards.html", billboards=result)
+
+
--- src/infrastructure/api/setup.py
+++ src/infrastructure/api/setup.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from infrastructure.container import Container
-from infrastructure.api.blueprints.queries import queries
 from infrastructure.api.blueprints.main import main
 from infrastructure.api.blueprints.auth import auth
 from infrastructure.api.blueprints.billboards import billboards
@@ -14,9 +13,8 @@ def init_config(container: Container):
 
 def setup(app: Flask, container: Container):
     init_config(container)
-    container.wire(modules=[queries, auth, billboards])
+    container.wire(modules=[auth, billboards])
     app.register_blueprint(main.main_blueprint, url_prefix="/")
-    app.register_blueprint(queries.query_blueprint, url_prefix="/queries")
     app.register_blueprint(auth.auth_blueprint, url_prefix="/auth")
     app.register_blueprint(billboards.billboard_blueprint, url_prefix="/billboards")
     app.template_folder = "infrastructure/api/base_templates"
